5466a7eb82542586d4cfdf461bb569a3462137aa
David Amar <davidama@CVMED-C02ZR073MD6V.local>
2020-06-29 00:12:25 -0700

    add bnlearn with boot

--- R/run_bnlearn_on_simulated_data.R
+++ R/run_bnlearn_on_simulated_data.R
@@ -13,8 +13,8 @@ for (lib_name in required_libs){
 
 # a function to add the known distances to the results
 add_distances<-function(m,dists,newcolname = "KnownDistance"){
-  m[,1] = as.character(m[,1])
-  m[,2] = as.character(m[,2])
+  m[,1] = as.character(m[,1]) # from, columns
+  m[,2] = as.character(m[,2]) # to, rows
   v = c()
   for(i in 1:nrow(m)){
     v = c(v,dists[m[i,2],m[i,1]]) # add dist from exposure to outcome
@@ -32,7 +32,9 @@ get_distance_based_metrics<-function(arc_dists,topK){
   return(c(fdr,length(arc_dists)))
 }
 
-get_bnlearn_results<-function(rdata,cut_breaks = 5,boot.reps=100){
+# if cut_breaks < 2, use a mixture of continuous and categorical 
+# variables
+get_bnlearn_results<-function(rdata,cut_breaks = -1,boot.reps=100,...){
   load(rdata)
   insts = grepl("IV",colnames(simulated_data))
   bls = c()
@@ -41,27 +43,43 @@ get_bnlearn_results<-function(rdata,cut_breaks = 5,boot.reps=100){
   }
   # bls = rbind(bls,
   #   cbind(colnames(simulated_data)[insts],colnames(simulated_data)[insts]))
-  m1 = apply(simulated_data[,!insts],2,cut,breaks=cut_breaks)
-  m2 = simulated_data[,insts]
-  df = as.data.frame(cbind(m1,m2))
+  
+  bic = "bic"
+  if(cut_breaks > 1){
+    m1 = apply(simulated_data[,!insts],2,cut,breaks=cut_breaks)
+    m2 = simulated_data[,insts]
+    df = as.data.frame(cbind(m1,m2))
+    
+  }
+  else{
+    df = as.data.frame(simulated_data)
+    for(j in which(insts)){
+      df[[j]] = as.factor(df[[j]])
+      bic = "bic-cg"
+    }
+  }
+  
   dags = list()
   for(j in 1:boot.reps){
     print(j)
     samp = sample(1:nrow(df),replace = T)
-    dags[[j]] = hc(df[samp,], score = "bic",blacklist = bls)
+    dags[[j]] = hc(df[samp,],blacklist = bls,score = bic,...)
   }
   t_arcs = c()
   for(j in 1:boot.reps){
     arcs = dags[[j]]$arcs
     arcs_t = arcs[! grepl("IV",arcs[,1]) & !grepl("IV",arcs[,2]),]
-    arcs_string = apply(arcs_t,1,paste,collapse = "->")
-    t_arcs = c(t_arcs,arcs_string)
+    if(length(arcs_t) != 0 && nrow(arcs_t)>1 ){
+      arcs_string = apply(arcs_t,1,paste,collapse = "->")
+      t_arcs = c(t_arcs,arcs_string)
+    }
   }
   tb = table(t_arcs) / length(dags)
   tb_n = sapply(names(tb),function(x)(strsplit(x,split="->")[[1]]))
   m = cbind(t(tb_n),tb)
+  # m = m[,c(2,1,3)]
   
-  if(length(m) != 0 && nrow(m)>0 ){
+  if(length(m) != 0 && nrow(m)>1 ){
     m = add_distances(m,B_distances)
     ord = order(as.numeric(m[,3]),decreasing = T)
     m = m[ord,]
@@ -71,7 +89,6 @@ get_bnlearn_results<-function(rdata,cut_breaks = 5,boot.reps=100){
     return(NULL)
   }
 }
-
 
 library(parallel)
 
@@ -89,7 +106,9 @@ for(deg in degs){
       rdata_files = rdata_files[grepl("rdata",rdata_files,ignore.case = T)]
       rdata_files = rdata_files[1:20]
       
-      res = mclapply(rdata_files,get_bnlearn_results,cut_breaks=5,mc.cores = 20,boot.reps=200)
+      res = mclapply(rdata_files,get_bnlearn_results,cut_breaks=-1,
+                     mc.cores = 20,boot.reps=20,restart=100)
+      names(res) = rdata_files
       
       k2scores = list()
       for(k in ks){
@@ -103,15 +122,21 @@ for(deg in degs){
         }
       }
       param2res[[paste0("deg",deg,"_pleio",pleio)]] = list(res,k2scores)
-      save(res,param2res,file="bnlearn_boot_breaks5.RData")
+      save(param2res,file="bnlearn_boot_cg.RData")
   }
 }
 
-# Create some plots locally
-load("~/Desktop/causal_inference_projects/ms3/simulations_default/bnlearn_param2res_breaks5.RData")
-load("~/Desktop/causal_inference_projects/ms3/simulations_default/bnlearn_param2res.RData")
-sapply(param2res,function(x)apply(x,2,median))
+# # Create some plots locally
+# load("~/Desktop/causal_inference_projects/ms3/simulations_default/bnlearn_param2res_breaks5.RData")
+# load("~/Desktop/causal_inference_projects/ms3/simulations_default/bnlearn_param2res.RData")
+# sapply(param2res,function(x)apply(x,2,median))
 
+setwd("/oak/stanford/groups/mrivas/users/davidama/cgauge_resub/simulations_default/")
+load("bnlearn_boot_breaks5.RData")
+for(nn in names(param2res)){
+  print(nn)
+  k2scores = param2res[[nn]][[2]]
+  print(t(sapply(k2scores,function(x)apply(x,2,mean))))
+}
 
-t(sapply(k2scores,function(x)apply(x,2,median)))
 
